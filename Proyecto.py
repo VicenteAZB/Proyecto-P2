8772d2ed2791161d73942e842ba11f092b1d45dc
VicenteAZB <vicente.alvarez2023@alu.uct.cl>
2023-11-26 00:42:52 -0300

    25NOV

--- Proyecto.py
+++ Proyecto.py
@@ -6,7 +6,7 @@ from pygame.locals import *
 
 nRES = (1184, 576)
 ok = True
-limitX = 37
+limitX = 36
 limitY = 13 
 celda = 32
 
@@ -37,6 +37,9 @@ def Carga_Imagenes():
     imagenes.append(Load_Image('leona.png', True))
     imagenes.append(Load_Image('cebra.png', True))
     imagenes.append(Load_Image('planta1.png', True))
+    imagenes.append(Load_Image('abeja.png', True))
+    imagenes.append(Load_Image('abeja2.png', True))
+    imagenes.append(Load_Image('cebra2.png', True))
     return imagenes
 
 class Organismo:
@@ -61,7 +64,6 @@ class Animal(Organismo):
     def cazar(self):
         self.energia += 30
         self.vida += 20
-        print("cazadooooooo")
 
     def reproducirse(self):
         cria = Animal(self.x + 32, self.y + 32, 100, 100, self.velocidad, self.especie, ra.choice(["Macho", "Hembra"]), self.dieta)
@@ -74,14 +76,14 @@ class Planta(Organismo):
         self.se_reproduce = se_reproduce
 
     def reproducirse(self):
-
-        pass
+        Planta(self, self.x + ra.choice([-32, 32]), self.x + ra.choice([-32, 32]), 100, 100, 0, self.especie, self.realiza_fotosintesis, self.se_reproduce)
+        
 
 class Ambiente:
-    def __init__(self, temperatura, humedad, eventos_climaticos):
+    def __init__(self, temperatura, humedad, clima):
         self.temperatura = temperatura
         self.humedad = humedad
-        self.eventos_climaticos = eventos_climaticos
+        self.clima = clima
 
     def afectar_ecosistema(self):
         pass
@@ -89,9 +91,14 @@ class Ambiente:
 class Ecosistema():
     def __init__(self):
         self.organismos = []
+        self.ambientes = []
+        self.tiempo = 0
 
     def agregar_organismo(self, organismo):
         self.organismos.append(organismo)
+
+    def agregar_ambientes(self, ambiente):
+        self.ambientes.append(ambiente)
 
     def gestionar_ciclo_de_vida(self):
         for i in self.organismos.copy():
@@ -118,9 +125,16 @@ class Ecosistema():
             if organismo1.dieta == "Carnivoro" and organismo2.dieta == "Hervívoro":
                 organismo1.cazar()
                 organismo2.vida -= 40
+
             elif organismo2.dieta == "Carnivoro" and organismo1.dieta == "Hervívoro":
                 organismo2.cazar()
                 organismo1.vida -40
+
+        if isinstance(organismo1, Animal) and isinstance(organismo2, Planta):
+            if organismo2.se_reproduce == "si" and organismo1.dieta == "Polen":
+                organismo2.reproducirse()
+                organismo1.vida += 20
+            
 
     def Reproducción(self, organismo1, organismo2):
         if isinstance(organismo1, Animal) and isinstance(organismo2, Animal):
@@ -132,16 +146,37 @@ class Ecosistema():
         pass
 
     def Pinta_Mapa(self, sWin, aFig):
-        for nF in range(0, nRES[1]//celda):
-            for nC in range(0, nRES[0]//celda):
-                if nF <= 448//celda and nC < 896//celda: 
-                    sWin.blit(aFig[0], (nC*celda , nF*celda))  
-                elif nF > 448//celda and nC < 896//celda:
-                    sWin.blit(aFig[2], (nC*celda , nF*celda))
-                else:
-                    sWin.blit(aFig[1], (nC*celda , nF*celda))
+            if ti.time() >= self.tiempo: 
+                clima = self.ambientes[ra.randint(0, 2)]
+                self.tiempo += 1
+
+            for nF in range(0, nRES[1]//celda):
+                for nC in range(0, nRES[0]//celda):
+                    if isinstance(clima, Ambiente):
+                        if clima.clima == "Sol":
+                            if nF <= 448//celda and nC < 896//celda: 
+                                sWin.blit(aFig[0], (nC*celda , nF*celda))  
+                            elif nF > 448//celda and nC < 896//celda:
+                                sWin.blit(aFig[2], (nC*celda , nF*celda))
+                            else:
+                                sWin.blit(aFig[1], (nC*celda , nF*celda))
+
+                        if clima.clima == "Lluvia":
+                            if nF <= 448//celda and nC < 896//celda: 
+                                sWin.blit(aFig[2], (nC*celda , nF*celda))  
+                            elif nF > 448//celda and nC < 896//celda:
+                                sWin.blit(aFig[1], (nC*celda , nF*celda))
+                            else:
+                                sWin.blit(aFig[0], (nC*celda , nF*celda))
         
-    
+                        if clima.clima == "Nieve":
+                            if nF <= 448//celda and nC < 896//celda: 
+                                sWin.blit(aFig[1], (nC*celda , nF*celda))  
+                            elif nF > 448//celda and nC < 896//celda:
+                                sWin.blit(aFig[2], (nC*celda , nF*celda))
+                            else:
+                                sWin.blit(aFig[0], (nC*celda , nF*celda))
+
     def Pinta_Organismos(self, sWin, aFig):
         for i in self.organismos:
             if i.especie == "León" and i.vida > 0 and i.genero == "Macho":
@@ -150,12 +185,21 @@ class Ecosistema():
             if i.especie == "León" and i.vida > 0 and i.genero == "Hembra":
                 sWin.blit(aFig[5], (i.x, i.y))
 
-            if i.especie == "Cebra" and i.vida > 0:
+            if i.especie == "Cebra" and i.vida > 0 and i.genero == "Macho":
                 sWin.blit(aFig[6], (i.x , i.y))
 
-            if i.especie == "Planta" and i.vida > 0:
+            if i.especie == "Cebra" and i.vida > 0 and i.genero == "Hembra":
+                sWin.blit(aFig[10], (i.x , i.y))
+
+            if i.especie == "Tallo" and i.vida > 0:
                 sWin.blit(aFig[7], (i.x , i.y))
-            
+
+            if i.especie == "Abeja" and i.vida > 0 and i.genero == "Macho":
+                sWin.blit(aFig[8], (i.x , i.y))
+
+            if i.especie == "Abeja" and i.vida > 0 and i.genero == "Hembra":
+                sWin.blit(aFig[9], (i.x , i.y))
+
             if isinstance(i, Animal):
                 i.moverse()
 
@@ -171,12 +215,24 @@ class Ecosistema():
 Leona = Animal(ra.randint(0,limitX)*32, ra.randint(0,limitY)*32, 100, 100, 1, "León", "Hembra", "Carnivoro")
 León = Animal(ra.randint(0,limitX)*32, ra.randint(0,limitY)*32, 100, 100, 1, "León", "Macho", "Carnivoro")
 Cebra = Animal(ra.randint(0,limitX)*32, ra.randint(0,limitY)*32, 100, 100, 1, "Cebra", "Macho", "Hervívoro")
-Planta1 = Planta(ra.randint(0,limitX)*32, ra.randint(0,limitY)*32, 100, 100, 0, "Planta", "si", "si")
+Cebra2 = Animal(ra.randint(0,limitX)*32, ra.randint(0,limitY)*32, 100, 100, 1, "Cebra", "Hembra", "Hervívoro")
+Tallo = Planta(ra.randint(0,limitX)*32, ra.randint(0,limitY)*32, 100, 100, 0, "Tallo", "si", "si")
+Abeja = Animal(ra.randint(0,limitX)*32, ra.randint(0,limitY)*32, 100, 100, 1, "Abeja", "Macho", "Polen")
+Abeja2 = Animal(ra.randint(0,limitX)*32, ra.randint(0,limitY)*32, 100, 100, 1, "Abeja", "Hembra", "Polen")
+Soleado = Ambiente(30, 25, "Sol")
+Lluvia = Ambiente(15, 90, "Lluvia")
+Nieve = Ambiente(-15, 100, "Nieve")
 Programa = Ecosistema()
 Programa.agregar_organismo(León)
 Programa.agregar_organismo(Leona)
 Programa.agregar_organismo(Cebra)
-Programa.agregar_organismo(Planta1)
+Programa.agregar_organismo(Cebra2)
+Programa.agregar_organismo(Tallo)
+Programa.agregar_organismo(Abeja)
+Programa.agregar_organismo(Abeja2)
+Programa.agregar_ambientes(Soleado)
+Programa.agregar_ambientes(Lluvia)
+Programa.agregar_ambientes(Nieve)
 sWin = Init_PyGame()
 aFig = Carga_Imagenes()
 reloj = py.time.Clock()
@@ -189,6 +245,10 @@ while ok:
     Programa.gestionar_ciclo_de_vida()
     Programa.gestionar_interacciones()
     Programa.Pinta_Organismos(sWin, aFig)
+
+    for organismo in Programa.organismos:
+        print(f"{organismo.especie} en la posición ({organismo.x}, {organismo.y}) - Vida: {organismo.vida}, Energía: {organismo.energia}")
+
     py.display.flip()
     reloj.tick(1)
     
